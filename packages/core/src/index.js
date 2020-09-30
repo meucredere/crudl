892d13e48f1305a3e790bd391f91ad3e9a3e539a
Kaio Cristian <kaiocristian@hotmail.com>
2020-09-30 12:53:50 -0300

    default crudls export improvements

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -7,17 +7,15 @@ import requestsGenerator from '@/generators/requests';
 import modifiersGenerator from '@/generators/modifiers';
 import dataGenerator from '@/generators/data';
 
-export default function generator(key, config = {}) {
-  const crudl = {};
-
-  crudl.operations = operationsGenerator(key, config, crudl);
-  crudl.keys = keysGenerator(key, config, crudl);
-  crudl.endpoints = endpointsGenerator(key, config, crudl);
-  crudl.methods = methodsGenerator(key, config, crudl);
-  crudl.constants = constantsGenerator(key, config, crudl);
-  crudl.requests = requestsGenerator(key, config, crudl);
-  crudl.modifiers = modifiersGenerator(key, config, crudl);
-  crudl.data = dataGenerator(key, config, crudl);
-
-  return crudl;
+export default function crudl(key, config = {}) {
+  return {
+    operations: operationsGenerator(key, config),
+    keys: keysGenerator(key, config),
+    endpoints: endpointsGenerator(key, config),
+    methods: methodsGenerator(key, config),
+    constants: constantsGenerator(key, config),
+    requests: requestsGenerator(key, config),
+    modifiers: modifiersGenerator(key, config),
+    data: dataGenerator(key, config),
+  };
 }
